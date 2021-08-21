adb4de882f8086de99f1863192fec6673a514515
mnonna <marcinnonna026@gmail.com>
2021-08-21 21:43:14 +0200

    New fields

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'portfolio'),
-        username: env('DATABASE_USERNAME', 'lebrone2021'),
-        password: env('DATABASE_PASSWORD', 'Drt545ncD'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'Drt545ncd'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
