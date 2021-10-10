b596701b06dab09359cd25afb17372ea9923b8c4
mnonna <marcinnonna026@gmail.com>
2021-10-10 12:35:35 +0200

    Changed smtp password to app pass

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,19 @@
 module.exports = ({ env }) => ({
-    email: {
-      provider: 'smtp',
-      providerOptions: {
-        host: 'smtp.gmail.com', //SMTP Host
-        port: 465   , //SMTP Port
-        secure: true,
-        username: 'mncsgo19@gmail.com',
-        password: 'marcin4455',
-        rejectUnauthorized: true,
-        requireTLS: true,
-        connectionTimeout: 1,
-      },
-      settings: {
-        from: 'mncsgo19@gmail.com',
-        replyTo: 'mncsgo19@gmail.com',
-      },
+  email: {
+    provider: 'smtp',
+    providerOptions: {
+      host: 'smtp.gmail.com', //SMTP Host
+      port: 465, //SMTP Port
+      secure: true,
+      username: 'mncsgo19@gmail.com',
+      password: 'cmegmqqjrcthcnjt',
+      rejectUnauthorized: true,
+      requireTLS: false,
+      connectionTimeout: 1,
     },
-  });
-  
+    settings: {
+      from: 'mncsgo19@gmail.com',
+      replyTo: 'mncsgo19@gmail.com',
+    },
+  },
+});
